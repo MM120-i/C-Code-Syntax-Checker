26f0e7d536b9babfc1cf5d39f4d5d9ff24c33e44
Mahim <80307451+MM120-i@users.noreply.github.com>
2024-02-22 18:22:01 -0500

    Update Checker.h

---
+++ src/Checker.h
@@ -0,0 +1,15 @@
+#ifndef CHECKER_H
+#define CHECKER_H
+
+#include "fileLine.h"  
+
+int printCheck(const char *line, int length);
+int scanCheck(const char *line, int length);
+int getsCheck(const char *line, int length);
+int putsCheck(const char *line, int length);
+int fscanfcheck(const char *line, int length);
+int fprintfcheck(const char *line, int length);
+int forCheck(const char *line, int length);
+int whileCheck(const char *line, int length);
+
+#endif  // CHECKER_H
